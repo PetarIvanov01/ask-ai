bccb38ff1b42731e83616c81ac9c4084a1946daa
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-16 18:05:39 +0300

    chore: add different color classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      transitionProperty: {
+        width: "width",
+      },
+      colors: {
+        "dark-gray-1": "#131617",
+        "darker-gray": "#0D0F10",
+        "dark-gray-2": "#121518",
+        "dark-gray-3": "#0F1113",
+        "dark-gray-4": "#14171A",
+        "dark-gray-5": "#15181B",
+        "gray-1": "#1A1E21",
+        "gray-2": "#191D20",
+        "gray-3": "#1B1F22",
+        "gray-4": "#111416",
+      },
     },
   },
   plugins: [],
