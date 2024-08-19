751d13d7e37815a8d9362cf747129ab314d320a3
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-19 23:13:58 +0300

    feat: add SideNavigation component with expandable sidebar
    - Currently, with test data only.

---
+++ src/components/navigation/SideNavigation.tsx
@@ -0,0 +1,164 @@
+"use client";
+import Image from "next/image";
+
+import { useState } from "react";
+
+export default function SideNavigation() {
+  const [isOpen, setOpen] = useState(true);
+  return (
+    <aside
+      className={`relative flex py-2 bg-dark-gray-1 rounded-md px-2 h-full`}
+    >
+      <button
+        type="button"
+        className={`bg-dark-gray-1 group items-center w-fit flex ${
+          isOpen ? "opacity-0 w-0 absolute top-1/2" : "opacity-100 w-full"
+        }`}
+        onClick={() => setOpen(true)}
+      >
+        <span className="sr-only">Open sidebar</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-8 scale-x scale-x-[-1] group-hover:ring-1 group-hover:ring-white rounded-lg p-1"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+          />
+        </svg>
+      </button>
+
+      <div
+        className={`flex flex-col transition-width duration-300 ease-in ${
+          isOpen
+            ? "translate-x-0 min-[240px]:w-[150px] sm:w-[200px]"
+            : "-translate-x-full w-0 opacity-0"
+        }`}
+      >
+        <ul className="flex flex-col gap-8 pt-1 px-1 text-nowrap overflow-hidden">
+          <li className="flex items-center justify-between">
+            <p className="uppercase text-sm">General</p>
+            <button
+              onClick={() => {
+                setOpen(false);
+              }}
+              className="block  hover:ring-1 hover:ring-white rounded-lg p-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+                />
+              </svg>
+            </button>
+          </li>
+          <li className="flex items-center gap-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="opacity-30 size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+            Home
+          </li>
+        </ul>
+
+        <div className="flex flex-col gap-4 pt-12 text-sm text-nowrap overflow-hidden">
+          <p className="opacity-30 text-sm">Today</p>
+          <ul className="flex flex-col gap-1 ">
+            <li className="bg-gradient-to-r  from-gray-600 shadow shadow-dark-gray-1 p-2 rounded-lg flex items-center gap-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6 text-green-300"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.5 8.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v8.25A2.25 2.25 0 0 0 6 16.5h2.25m8.25-8.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-7.5A2.25 2.25 0 0 1 8.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 0 0-2.25 2.25v6"
+                />
+              </svg>
+              JavaScript - Runtime
+            </li>
+
+            <li className="p-2 gap-3 flex items-center ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6 text-red-400"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+                />
+              </svg>
+              Front-End Concepts
+            </li>
+          </ul>
+        </div>
+
+        <div className="mt-auto flex items-center rounded-xl p-2 shadow bg-gradient-to-r shadow-black text-sm text-nowrap overflow-hidden">
+          <div>
+            <Image
+              src="/prof-icon-2.png"
+              alt="profile icon"
+              width={40}
+              height={40}
+            />
+          </div>
+
+          <p className="pl-3 mr-auto">Petar Ivanov</p>
+          <div className="opacity-30">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+}
