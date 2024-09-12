ee6c3d5bff2770632475558a735af4d7486b5832
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-12 20:34:35 +0300

    feat: wrap the page with main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
-export default function GenericPage() {
+import Categories from "../ui/entry-page/categories";
+import MainLayout from "./main-layout";
+
+export default function EntryPage() {
   return (
-    <>
-      <h2 className="text-xl font-bold mb-4">
-        Learn Programming Concepts with AI
-      </h2>
-      <p className="mb-4">
-        Improve your theoretical knowledge through AI conversations.
-      </p>
-      <p>Select a category to get started.</p>
-      {/* Add categories for different concepts and languages ....  */}
-    </>
+    <MainLayout>
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-xl font-bold mb-4">
+          Learn Programming Concepts with AI
+        </h2>
+        <p className="mb-4">
+          Improve your theoretical knowledge through AI conversations.
+        </p>
+
+        <Categories />
+      </div>
+    </MainLayout>
   );
 }
