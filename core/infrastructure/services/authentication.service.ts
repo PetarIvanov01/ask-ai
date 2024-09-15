6cb42f0ec83175adaddaa740a16352a9d395d1b1
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-15 21:45:11 +0300

    refactor: - implement logout method - fix input types - Due to supabase provided functionality, I make a database procedure that enables on registration to create a profile entity in profiles table

--- core/infrastructure/services/authentication.service.ts
+++ core/infrastructure/services/authentication.service.ts
@@ -3,7 +3,7 @@ import { injectable } from "inversify";
 import { IAuthenticationService } from "@/core/application/services/authentication.service.interface";
 
 import { Session, sessionSchema } from "@/core/entities/models/session";
-import { UserAuth } from "@/core/entities/models/user";
+import { UserSignUp, UserSignIn } from "@/core/entities/models/user";
 
 import { createClient } from "../utils/supabase/server";
 import { AuthenticationError } from "@/core/entities/custom-errors/errors";
@@ -11,7 +11,12 @@ import { AuthenticationError } from "@/core/entities/custom-errors/errors";
 // Here is the actual implementation of the Authentication service which is depends on a interface
 @injectable()
 export class AuthenticationService implements IAuthenticationService {
-  async createSessionOnSignIn(user: UserAuth): Promise<{ session: Session }> {
+  async clearSessionOnLogout(): Promise<void> {
+    const supabase = createClient();
+
+    await supabase.auth.signOut();
+  }
+  async createSessionOnSignIn(user: UserSignIn): Promise<{ session: Session }> {
     const supabase = createClient();
 
     const { data, error } = await supabase.auth.signInWithPassword(user);
@@ -19,6 +24,7 @@ export class AuthenticationService implements IAuthenticationService {
     if (error) {
       throw new AuthenticationError(error.message);
     }
+
     const session = sessionSchema.parse({
       token: data.session?.access_token,
       userId: data.user?.id,
@@ -30,12 +36,24 @@ export class AuthenticationService implements IAuthenticationService {
     };
   }
 
-  async createSessionOnSignUp(user: UserAuth): Promise<{ session: Session }> {
+  async createProfileOnSignUp(user: UserSignUp): Promise<{ session: Session }> {
     const supabase = createClient();
 
-    const { data, error } = await supabase.auth.signUp(user);
+    const { data, error } = await supabase.auth.signUp({
+      email: user.email,
+      password: user.password,
+      options: {
+        data: {
+          username: user.username,
+        },
+      },
+    });
 
     if (error) {
+      if (error.status) {
+        throw new AuthenticationError("User with this email already exist.");
+      }
+
       throw new AuthenticationError(error.message);
     }
 
