414b00c0a6f15e3b3958e20bdcd90b35a4e701f6
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-15 21:47:43 +0300

    feat: implement category repository - getIcon method construct the public url for a stored file in supabase - getCategories fetches the stored in categories table items

---
+++ core/infrastructure/repositories/category.repository.ts
@@ -0,0 +1,29 @@
+import { injectable } from "inversify";
+
+import type { ICategoryRepository } from "@/core/application/repositories/category.repository.interface";
+import { Icon, type Category } from "@/core/entities/models/category";
+import { createClient } from "../utils/supabase/server";
+
+@injectable()
+export class CategoryRepository implements ICategoryRepository {
+  getIcon(fileName: string): Icon {
+    const supabase = createClient();
+    const { data: imageUrl } = supabase.storage
+      .from("categories-icons")
+      .getPublicUrl(fileName);
+
+    return { url: imageUrl.publicUrl };
+  }
+  async getCategories(): Promise<Category[]> {
+    const supabase = createClient();
+
+    const response = await supabase.from("categories").select();
+
+    const { data: cardsData, error } = response;
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return cardsData;
+  }
+}
