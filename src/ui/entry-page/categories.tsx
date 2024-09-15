c7638a6dc0c92050b3b6de38000ab7cfcab05662
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-15 20:51:29 +0300

    feat: add dynamic data fetching for categories, replacing static data, and render fetched icons using getIconURL.

--- src/ui/entry-page/categories.tsx
+++ src/ui/entry-page/categories.tsx
@@ -1,13 +1,23 @@
-import { ReactNode } from "react";
-import { cardsData } from "./cards-data";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Categories() {
+import {
+  getCategories,
+  getIconURL,
+} from "@/core/interface-adapters/controllers/category.controller";
+
+const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export default async function Categories() {
+  const cardsData = await getCategories();
+
+  await wait(2000);
   return (
     <div className="flex justify-center items-center flex-wrap">
       {cardsData.map((card, index) => (
         <Card
           key={index}
-          image={card.image}
+          image={getIconURL(card.iconUrl)}
           title={card.title}
           options={card.options}
         />
@@ -17,7 +27,7 @@ export default function Categories() {
 }
 
 type CardProps = Readonly<{
-  image: ReactNode;
+  image: string;
   title: string;
   options: string[];
 }>;
@@ -28,17 +38,18 @@ const Card = ({ image, title, options }: CardProps) => {
       <div
         className={`size-14 rounded-full mb-4 bg-darker-gray  flex justify-center items-center`}
       >
-        {image}
+        <Image width={50} height={50} src={image} alt={title} />
       </div>
       <h2 className="text-xl font-semibold mb-4">{title}</h2>
       <div className="w-60">
         {options.map((option, index) => (
-          <div
+          <Link
+            href={`/chat/${option.toLowerCase().replace(/\/| /g, "-")}`}
             key={index}
             className="flex justify-between items-center shadow-lg text-sm bg-darker-gray p-4 mb-2 rounded-lg cursor-pointer hover:bg-dark-gray-1 transition-colors duration-200"
           >
             {option} <span className="text-gray-600">→</span>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
