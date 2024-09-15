4ee11b1415abb7b31278111fbd3ff31237a089c7
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-15 21:34:22 +0300

    fix: remove 'createUserProfile' method

--- core/application/repositories/users.repository.interface.ts
+++ core/application/repositories/users.repository.interface.ts
@@ -2,14 +2,6 @@ import { UserProfile } from "@/core/entities/models/user";
 
 export interface IUsersRepository {
   getUser(id: string): Promise<UserProfile | undefined>;
-  createUserProfile({
-    username,
-    email,
-  }: {
-    username: string;
-    email: string;
-  }): Promise<{ username: string; email: string }>;
-
   getUserByUsername(username: string): Promise<UserProfile | undefined>;
   getUserByEmail(email: string): Promise<UserProfile | undefined>;
 }
