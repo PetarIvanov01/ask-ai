c146b093b8e15f682ee148a99ecfca518f6935e0
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-03 23:57:51 +0300

    refactor: remove message provider and pass username to the side bar

--- src/app/main-layout.tsx
+++ src/app/main-layout.tsx
@@ -1,29 +1,34 @@
 import { ReactNode } from "react";
 
-import MessageInput from "../ui/message-input";
 import SideNavigation from "../ui/navigation/side-bar";
 import SideBarContent from "../ui/navigation/side-bar-content";
 import ChatHeader from "./chat-header";
 
-import MessageProvider from "../context/MessageContext";
+import { getSession } from "@/core/interface-adapters/controllers/authentication/session.controller";
+import MessageInputStatic from "../ui/message-input-static";
 
-export default function MainLayout({ children }: { children: ReactNode }) {
+export default async function MainLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getSession();
+
   return (
     <div className="flex h-screen bg-gray-2 text-white">
       <div className="w-full flex flex-grow p-2 gap-2">
         <SideNavigation>
-          <SideBarContent />
+          <SideBarContent username={session.username} />
         </SideNavigation>
 
         <section className="w-full justify-between max-h-screen flex flex-col flex-grow mx-auto">
           <ChatHeader />
-          <MessageProvider>
-            <div className="flex-grow h-full mt-3 flex flex-col justify-center items-center overflow-y-auto">
-              {children}
-            </div>
 
-            <MessageInput />
-          </MessageProvider>
+          <div className="flex-grow h-full mt-3 flex flex-col justify-center items-center overflow-y-auto">
+            {children}
+          </div>
+
+          <MessageInputStatic />
         </section>
       </div>
     </div>
