751076eedcddee40ca264b1d0a9cb7407c4b12da
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-04 21:56:39 +0300

    refac: wrap the component with mainlayout

--- src/app/chat/[chat]/layout.tsx
+++ src/app/chat/[chat]/layout.tsx
@@ -1,13 +1,17 @@
+import MainLayout from "../../main-layout";
+
 export default function layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <section className="flex-grow py-2 h-full flex flex-col justify-center items-center w-full">
-      <div className="flex w-full h-full flex-grow">
-        <div className="h-full w-full">{children}</div>
-      </div>
-    </section>
+    <MainLayout>
+      <section className="flex-grow pt-2 h-full flex flex-col justify-center items-center w-full">
+        <div className="flex w-full h-full flex-grow">
+          <div className="h-full w-full">{children}</div>
+        </div>
+      </section>
+    </MainLayout>
   );
 }
