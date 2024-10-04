8c73f6685d26a1ca1cefc9d3e3ae843fc3b66bab
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-04 21:55:22 +0300

    feat: create client component that displays the messages and handle upcoming messages bot preserving them in state and database through requests

---
+++ src/app/chat/[chat]/chat-window.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { useRef, useState } from "react";
+
+import MessageInput from "@/src/ui/message-input";
+import BotMessage from "./bot-message";
+import UserMessage from "./user-message";
+
+import { ChatSchema } from "@/core/entities/models/chat";
+import { createBotMessageAction, createUserMessageAction } from "./actions";
+
+type Message = ChatSchema["messages"][number];
+
+export default function ChatWindow({
+  initialMessages,
+  chatId,
+}: {
+  initialMessages: Message[];
+  chatId: string;
+}) {
+  const focusRef = useRef<HTMLDivElement>(null);
+  const onSubmitFocusMessage = () => {
+    focusRef.current?.scrollTo(0, focusRef.current.scrollHeight);
+  };
+
+  const [messages, setMessages] = useState(initialMessages);
+
+  const handleSendMessage = async (userInput: string) => {
+    const userMessage: Message = await createUserMessageAction(
+      userInput,
+      chatId
+    );
+
+    setMessages((prevMessages) => [...prevMessages, userMessage]);
+    try {
+      const botMessage = await createBotMessageAction(userInput, chatId);
+
+      setMessages((prevMessages) => [...prevMessages, botMessage]);
+    } catch (error) {
+      console.error("Error getting AI response:", error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col text-sm h-full">
+      <div ref={focusRef} className="flex-grow overflow-auto">
+        {messages.map((message) =>
+          message.role === "user" ? (
+            <UserMessage message={message.message} key={message.messageId} />
+          ) : (
+            <BotMessage message={message.message} key={message.messageId} />
+          )
+        )}
+      </div>
+
+      <MessageInput
+        onSubmitFocusMessage={onSubmitFocusMessage}
+        handleSendMessage={handleSendMessage}
+      />
+    </div>
+  );
+}
