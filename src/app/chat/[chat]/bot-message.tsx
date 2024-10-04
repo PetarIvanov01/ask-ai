bb6a56b2cc7b9a01dcb183d1d9490476e1a14464
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-04 21:56:17 +0300

    style: move the icon on the left top

--- src/app/chat/[chat]/bot-message.tsx
+++ src/app/chat/[chat]/bot-message.tsx
@@ -6,7 +6,7 @@ export default function BotMessage({ message }: { message: string }) {
     <article className="w-full text-token-text-primary focus-visible:outline-2 focus-visible:outline-offset-[-4px]">
       <div className="text-base py-[18px] px-3 m-auto md:px-5">
         <div className="mx-auto flex items-center flex-1 gap-4 text-base md:gap-5 lg:gap-6 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]">
-          <div className="flex-shrink-0 flex flex-col relative items-end">
+          <div className="flex-shrink-0 flex flex-col relative self-start pt-2">
             <div className="pt-0">
               <div className="flex h-10 w-10 items-center justify-center overflow-hidden rounded-full">
                 <div className="relative p-1 rounded-sm flex items-center justify-center bg-token-main-surface-primary text-token-text-primary">
