620becd2ab524da4c891c3ebb7327749aa6e00ee
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-05 22:02:35 +0300

    feat: add method "startCoversation" to the interface

--- core/application/services/ai.service.interface.ts
+++ core/application/services/ai.service.interface.ts
@@ -2,5 +2,6 @@ import { BotMessageShema } from "@/core/entities/models/chat";
 
 // Only the interface that will be implemented inside the infrastructure part.
 export interface IAiService {
+  startConversation: (topic: string) => Promise<BotMessageShema>;
   createBotResponse: (message: string) => Promise<BotMessageShema>;
 }
