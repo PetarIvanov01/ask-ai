b674590e9ba561f95d0375e3583a03b71f94255b
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-05 21:58:32 +0300

    refac: move create message controllers into own file

---
+++ core/interface-adapters/controllers/messages/messages.controller.ts
@@ -0,0 +1,45 @@
+import { getInjection } from "@/core/di/container";
+
+import { AuthenticationError } from "@/core/entities/custom-errors/errors";
+import { Message } from "@/core/entities/models/chat";
+
+import { createBotMessageUseCase } from "@/core/application/use-cases/chat/create-bot-message.use-case";
+import { createUserMessageUseCase } from "@/core/application/use-cases/chat/create-user-message.use-case";
+
+export async function createBotMessageController(
+  message: string,
+  chatId: string
+): Promise<Message> {
+  const authService = getInjection("IAuthenticationService");
+  const session = await authService.getSession();
+
+  if (!session) {
+    throw new AuthenticationError("Not Authenticated");
+  }
+
+  const data = await createBotMessageUseCase(message, chatId);
+  return {
+    message: data.message,
+    messageId: data.messageId,
+    role: "ai",
+  };
+}
+
+export async function createUserMessageController(
+  message: string,
+  chatId: string
+): Promise<Message> {
+  const authService = getInjection("IAuthenticationService");
+  const session = await authService.getSession();
+
+  if (!session?.userId) {
+    throw new AuthenticationError("Not Authenticated");
+  }
+
+  const data = await createUserMessageUseCase(message, chatId);
+  return {
+    message: data.message,
+    messageId: data.messageId,
+    role: "user",
+  };
+}
