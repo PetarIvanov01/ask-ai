589c04a93be4fc96c0b014acc3641791fffacccc
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-08 23:37:55 +0300

    fix types

--- core/dto/category.ts
+++ core/dto/category.ts
@@ -1,13 +1,20 @@
 import { Category } from "../entities/models/category";
-import { Database } from "../infrastructure/utils/supabase/database.gen";
 
-export function mapToCategoryDTO(
-  supabaseTablesResult: Database["public"]["Tables"]["categories"]["Row"]
-): Category {
+export function mapToCategoryDTO<T>(supabaseTablesResult: {
+  id: number;
+  created_at: string;
+  title: string;
+  icon_url: string;
+  category_options: {
+    id: number;
+    topic: string;
+    category_id: number | null;
+  }[];
+}): Category {
   return {
     id: supabaseTablesResult.id,
     iconUrl: supabaseTablesResult.icon_url,
-    options: supabaseTablesResult.options,
+    options: supabaseTablesResult.category_options.map((e) => e.topic),
     title: supabaseTablesResult.title,
   };
 }
