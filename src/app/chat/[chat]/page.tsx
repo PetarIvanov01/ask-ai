fee0d500129e1c76f63c05395249c0288c9a1aa6
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-08 23:19:11 +0300

    refac: normalize the req.params and pass it

--- src/app/chat/[chat]/page.tsx
+++ src/app/chat/[chat]/page.tsx
@@ -1,10 +1,19 @@
 import { getConversationController } from "@/core/interface-adapters/controllers/chat/chat.controller";
 
 import ChatWindow from "./chat-window";
+import { normalizeString } from "@/core/infrastructure/utils/string";
 
 export default async function ChatRoom({
   params,
 }: Readonly<{ params: { chat: string } }>) {
-  const chat = await getConversationController(params.chat);
-  return <ChatWindow initialMessages={chat.messages} chatId={chat.chatId} />;
+  const normalizeChatStr = normalizeString(params.chat);
+  const chat = await getConversationController(normalizeChatStr);
+
+  return (
+    <ChatWindow
+      topic={normalizeChatStr}
+      initialMessages={chat.messages}
+      chatId={chat.chatId}
+    />
+  );
 }
