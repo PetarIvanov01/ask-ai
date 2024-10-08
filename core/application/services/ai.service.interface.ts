1a61999a0ae8b6d644797f1f08941794183f02a3
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-08 23:31:52 +0300

    fix: send instructions to the ai when starting a conversation and creating a response

--- core/application/services/ai.service.interface.ts
+++ core/application/services/ai.service.interface.ts
@@ -2,6 +2,9 @@ import { BotMessageShema } from "@/core/entities/models/chat";
 
 // Only the interface that will be implemented inside the infrastructure part.
 export interface IAiService {
-  startConversation: (topic: string) => Promise<BotMessageShema>;
-  createBotResponse: (message: string) => Promise<BotMessageShema>;
+  startConversation: (instructions: string) => Promise<BotMessageShema>;
+  createBotResponse: (
+    message: string,
+    instructions: string
+  ) => Promise<BotMessageShema>;
 }
