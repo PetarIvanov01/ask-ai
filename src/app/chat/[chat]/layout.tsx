b1aa863ae1960c15b843b29d15e84c16b5eb642f
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-13 15:41:20 +0300

    refactor: fetch the session and pass props to the layouts

--- src/app/chat/[chat]/layout.tsx
+++ src/app/chat/[chat]/layout.tsx
@@ -2,21 +2,26 @@ import { Suspense } from "react";
 
 import LoadingFallback from "./LoadingSkeleton";
 import MainLayout from "../../main-layout";
+import ChatRoomLayout from "../../chat-room-layout";
+import { getSession } from "@/core/interface-adapters/controllers/authentication/session.controller";
 
-export default function layout({
+export default async function layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getSession();
   return (
-    <MainLayout>
-      <section className="flex-grow pt-2 h-full flex flex-col justify-center items-center w-full">
-        <div className="flex w-full h-full flex-grow">
-          <Suspense fallback={<LoadingFallback />}>
-            <div className="h-full w-full">{children}</div>
-          </Suspense>
-        </div>
-      </section>
+    <MainLayout username={session.username}>
+      <ChatRoomLayout userId={session.userId}>
+        <section className="flex-grow pt-2 h-full flex flex-col justify-center items-center w-full">
+          <div className="flex w-full h-full flex-grow">
+            <Suspense fallback={<LoadingFallback />}>
+              <div className="h-full w-full">{children}</div>
+            </Suspense>
+          </div>
+        </section>
+      </ChatRoomLayout>
     </MainLayout>
   );
 }
