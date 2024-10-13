ed0e03f0dd99d705a7d48706950aed41dca3c298
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-13 15:47:55 +0300

    refactor: fetch the session and wrap the entry page with ChatRoomLayout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,32 @@
 import { Suspense } from "react";
 
+import { getSession } from "@/core/interface-adapters/controllers/authentication/session.controller";
+
 import MainLayout from "./main-layout";
 
 import Categories from "../ui/entry-page/categories";
 import { SkeletonLoader } from "../ui/entry-page/skeleton";
+import ChatRoomLayout from "./chat-room-layout";
 
-export default function EntryPage() {
+export default async function EntryPage() {
+  const session = await getSession();
+
   return (
-    <MainLayout>
-      <div className="flex flex-col items-center justify-center">
-        <h2 className="text-xl font-bold mb-4">
-          Learn Programming Concepts with AI
-        </h2>
-        <p className="mb-4">
-          Improve your theoretical knowledge through AI conversations.
-        </p>
+    <MainLayout username={session.username}>
+      <ChatRoomLayout userId={session.userId}>
+        <div className="flex flex-col items-center justify-center">
+          <h2 className="text-xl font-bold mb-4">
+            Learn Programming Concepts with AI
+          </h2>
+          <p className="mb-4">
+            Improve your theoretical knowledge through AI conversations.
+          </p>
 
-        <Suspense fallback={<SkeletonLoader />}>
-          <Categories />
-        </Suspense>
-      </div>
+          <Suspense fallback={<SkeletonLoader />}>
+            <Categories />
+          </Suspense>
+        </div>
+      </ChatRoomLayout>
     </MainLayout>
   );
 }
