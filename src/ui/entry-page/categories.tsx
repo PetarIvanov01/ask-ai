f0bc0f7cd32536e7908c387ddeae7147472a871b
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-28 23:29:59 +0200

    refactor: extract the card map into CardList component

--- src/ui/entry-page/categories.tsx
+++ src/ui/entry-page/categories.tsx
@@ -2,22 +2,16 @@ import {
   getCategories,
   getIconURL,
 } from "@/core/interface-adapters/controllers/category.controller";
-import Card from "./card";
+import CardList from "./card-list";
 
 export default async function Categories() {
   const cardsData = await getCategories();
 
   return (
     <div className="flex justify-center items-center flex-wrap">
-      {cardsData.map((card, index) => (
-        <Card
-          key={index}
-          image={getIconURL(card.iconUrl)}
-          title={card.title}
-          id={card.id}
-          options={card.options}
-        />
-      ))}
+      <CardList
+        data={cardsData.map((e) => ({ ...e, image: getIconURL(e.iconUrl) }))}
+      />
     </div>
   );
 }
